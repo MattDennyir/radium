MattDennyir/radium

commit 874ec7908b68f5483501734a25e08bd794a2735c

Stop mutating style state
Fixes #236

modules/resolve-styles.js
@@ -7,7 +7,6 @@ var Config = require('./config');
 
 var ExecutionEnvironment = require('exenv');
 var React = require('react');
-var objectAssign = require('object-assign');
 
 // babel-eslint 3.1.7 fails here for some reason, error:
 //   0:0  error  Cannot call method 'isSequenceExpression' of undefined
@@ -30,9 +29,8 @@ var _getStyleState = function (component, key, value) {
 
 var _setStyleState = function (component, key, newState) {
   var existing = component.state && component.state._radiumStyleState || {};
-  var state = { _radiumStyleState: objectAssign({}, existing) };
-  state._radiumStyleState[key] = state._radiumStyleState[key] || {};
-  objectAssign(state._radiumStyleState[key], newState);
+  var state = { _radiumStyleState: {...existing} };
+  state._radiumStyleState[key] = {...state._radiumStyleState[key], ...newState};
   component.setState(state);
 };
 

modules/__tests__/resolve-styles-test.js
@@ -356,6 +356,29 @@ describe('resolveStyles', function () {
         result = resolveStyles(component, renderedElement);
         expect(result.props.style.background).toEqual('blue');
       });
+
+      it('doesn\'t mutate state', function () {
+        var component = genComponent();
+        var style = {background: 'blue'};
+        style[':' + pseudo] = {background: 'red'};
+        var renderedElement = <div style={style} />;
+
+        var result = resolveStyles(component, renderedElement);
+
+        // Capturing a reference to the existing state is enough, since Radium
+        // MUST return a new copy for shouldComponentUpdate.
+        var previousState = component.state._radiumStyleState;
+        result.props[onHandlerName]();
+        // If they are still equal here, that means we mutated the existing
+        // state, which will break shouldComponentUpdate.
+        expect(component.state._radiumStyleState).not.toEqual(previousState);
+
+        result = resolveStyles(component, renderedElement);
+
+        previousState = component.state._radiumStyleState;
+        result.props[offHandlerName]();
+        expect(component.state._radiumStyleState).not.toEqual(previousState);
+      });
     }
 
     it('calls existing ' + onHandlerName + ' handler', function () {

commit da9b73bdeb0a97d50120fa189ff6bb2259e81e64

Fix setStyleState regression in 874ec79
Oh React. If you call `setState` twice during the same execution frame,
the second time, `this.state` will be the same as the first time. Thus,
the second time will clobber the changes made the first time. This is a
known issue: https://github.com/facebook/react/issues/122

Btw, I noticed this when running the examples and trying the one with
all the boxes; mouseLeave on one box gets called in the same frame as
mouseEnter on another, and since all the boxes are in one component,
they clobbered each others state, and everyone stayed hovered. Too lazy
to write a proper test for this right now.

modules/resolve-styles.js
@@ -28,9 +28,13 @@ var _getStyleState = function (component, key, value) {
 };
 
 var _setStyleState = function (component, key, newState) {
-  var existing = component.state && component.state._radiumStyleState || {};
+  var existing = component._lastRadiumState ||
+    component.state && component.state._radiumStyleState || {};
+
   var state = { _radiumStyleState: {...existing} };
   state._radiumStyleState[key] = {...state._radiumStyleState[key], ...newState};
+
+  component._lastRadiumState = state;
   component.setState(state);
 };
 